3f3ef22f255ce5beb79b2a2da0ee179b9dc9ff58
gabe <ggetzie@gmail.com>
2020-12-13 19:34:56 +0800

    add bootstrap

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -9,13 +9,27 @@ import PropTypes from 'prop-types';
 class SearchForm extends Component {
   constructor( props ) {
     super( props );
-    this.state = { terms: '' };
+    this.state = { 
+      terms: '' ,
+      author: '',
+    };
     this.handleChange = this.handleChange.bind( this );
     this.handleSubmit = this.handleSubmit.bind( this );
+    this.clearForm = this.clearForm.bind( this );
   }
 
   handleChange( event ) {
     this.setState({ [event.target.name]: event.target.value });
+  }
+
+  clearForm( event ) {
+    event.preventDefault();
+    console.log("clearing form")
+    console.log(this.state)
+    for (let k in this.state) {
+      console.log(k);
+      this.setState({[k]: ""});
+    }
   }
 
   handleSubmit( event ) {
@@ -34,15 +48,36 @@ class SearchForm extends Component {
     if (this.props.loading) {
       content = <div className="spinner"></div>
     } else {
-      content = (<form onSubmit={this.handleSubmit}>
-                        <input type="text" value={this.state.terms} onChange={this.handleChange} name="terms" />
-                        <input type="submit" value="Submit" />
-                      </form>)
+      content = (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label for="terms">Keywords</label>
+          <input className="form-control" type="text" value={this.state.terms} onChange={this.handleChange} name="terms" />
+        </div>
+        <div className="form-row">
+          <div className="form-group col">
+            <label for="author">Author</label>
+            <input className="form-control" type="text" value={this.state.author} onChange={this.handleChange} name="author" />
+          </div>
+          <div className="form-group col">
+            <label for="year">Year</label>
+            <input className="form-control" type="number" value={this.state.year} onChange={this.handleChange} name="year" />
+          </div>
+        </div>
+        <div className="d-flex flex-row-reverse">
+          <input className="btn btn-primary" type="submit" value="Submit" />
+          <button className="btn btn-secondary mr-2" role="button" onClick={this.clearForm}>Clear</button>
+        </div>
+      </form>
+      )
     }
     
     return (
       <div className="search-form">
-        <h2>Search</h2>
+        <h2 className="mt-2">Search</h2>
+        <p>
+          Use the form below to search over 70M scholarly papers and display collaboration relationships on the map.
+        </p>
           {content}
       </div>
     );
