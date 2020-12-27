02579d7d27752471795074677880717191e2ec6a
gabe <ggetzie@gmail.com>
2020-12-27 20:33:05 +0800

    location search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -9,16 +9,6 @@ import { debounce } from "lodash";
 const ES_URL = 'https://1z85a4how2.execute-api.us-east-1.amazonaws.com/search_es'
 const DEBOUNCE_DELAY = 500;
 
-class CityList extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      cities: props.cities,
-      selected: -1,
-    }
-  }
-}
-
 class CitySearch extends Component {
   constructor (props) {
     super(props);
@@ -35,6 +25,9 @@ class CitySearch extends Component {
     this.delayedCallback = debounce(this.fetchItems, DEBOUNCE_DELAY);
     this.handleChange = this.handleChange.bind(this);
     this.fetchItems = this.fetchItems.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.getCity = this.getCity.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   handleChange(event) {
@@ -61,16 +54,20 @@ class CitySearch extends Component {
             fields: [
               "name",
               "name._2gram",
-              "name._3gram"
+              "name._3gram",
+              "asciiname",
+              "asciiname._2gram",
+              "asciiname._3gram"
             ]
           }
         },
         sort: [{population: "desc"}]
       }
       const res = await axios.post(ES_URL, data);
-      console.log("search cities")
-      console.log(res)
-      this.setState({cities: res.data.hits.hits});
+      this.setState({
+        cities: res.data.hits.hits,
+        focused: 0,
+      });
     } catch (error) {
       this.setState({
         error: {
@@ -83,16 +80,83 @@ class CitySearch extends Component {
     }
   } 
 
+  getCity(cityId) {
+    const cities = this.state.cities;
+    for (let city of cities) {
+      if (city._id === cityId) {
+        return city;
+      }
+    }
+    throw "City not found";
+  }
+
+  handleClick(event){
+    const cityId = event.target.attributes["data-city-id"].value;
+    const city = this.getCity(cityId);
+    const {name, admin1, country, location} = city._source
+    this.setState({
+      selected: city,
+      value: `${name}, ${admin1}, ${country}`,
+      cities: [],
+    })
+    this.props.setLocation(location);
+  }
+
+  handleKeyDown(event) {
+    const cf = this.state.focused;
+    const cityLen = this.state.cities.length;
+    if (event.keyCode === 40 || event.keyCode === 9) {
+      // Down arrow or TAB key
+        event.preventDefault();
+        this.setState({
+          focused: cf === cityLen - 1 ? 0 : cf + 1
+        })
+    } else if (event.keyCode === 38) {
+      // Up arrow
+      event.preventDefault();
+      this.setState({
+          focused: cf === 0 ? cityLen - 1 : cf - 1
+      })
+    } else if (event.keyCode === 13) {
+      // enter key - same as clicking on the selection
+      event.preventDefault();
+      const city = this.state.cities[cf]
+      const cityDiv = document.querySelector(`div[data-city-id='${city._id}']`);
+      cityDiv.click();
+    }
+  }
+
   render () {
-    let cityOptions = this.state.cities.map(city => (
-      <div key={city._id} className="ac_option">{city._source.name}, {city._source.admin1}, {city._source.country}</div>
+    const fi = this.state.focused;
+    let cityOptions = this.state.cities.map((city, i) => (
+      <div key={city._id} className={"ac_option" + (i === fi ? " ac_active" : "")} onClick={this.handleClick} data-city-id={city._id}>
+        {city._source.name}, {city._source.admin1}, {city._source.country}
+      </div>
     ))
+    let loadingMsg = (
+      <div>
+        Searching for cities…
+      </div>
+    )
+    let content;
+    const { isLoading } = this.state;
+    if (isLoading) {
+      content = loadingMsg;
+    } else {
+      content = cityOptions;
+    }
     return (
       <div className="form-group">
         <label htmlFor="city">City</label>
-        <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange} name="city" />
+        <input 
+          type="text" 
+          className="form-control" 
+          value={this.state.value} 
+          onChange={this.handleChange} 
+          onKeyDown={this.handleKeyDown} 
+          name="city" />
         <div className="ac_container">
-          {cityOptions}
+          {content}
         </div>
       </div>
     )
@@ -108,15 +172,20 @@ class SearchForm extends Component {
       author: "",
       minYear: "",
       maxYear: "",
-      cities: [],
+      location: [],
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.clearForm = this.clearForm.bind(this);
+    this.setLocation = this.setLocation.bind(this);
   }
 
   handleChange(event) {
     this.setState({[event.target.name]: event.target.value});
+  }
+
+  setLocation(location) {
+    this.setState({location: location})
   }
 
   clearForm( event ) {
@@ -126,6 +195,7 @@ class SearchForm extends Component {
       author: "",
       minYear: "",
       maxYear: "",
+      location: [],
     })
   }
 
@@ -146,7 +216,11 @@ class SearchForm extends Component {
       must_items.push({match: {"authors.author_name": this.state.author}});
     }
     if (this.state.minYear && this.state.maxYear) {
-      filter_items = [{range: {year: {gte: this.state.minYear, lte:this.state.maxYear}}}]
+      filter_items.push({range: {year: {gte: this.state.minYear, lte:this.state.maxYear}}})
+    }
+
+    if (this.state.location.length === 2) {
+      filter_items.push({geo_distance: {distance: "100km", locations: this.state.location}})
     }
     const query = {
       bool: {
@@ -183,7 +257,7 @@ class SearchForm extends Component {
             <input className="form-control" type="number" value={this.state.maxYear} onChange={this.handleChange} name="maxYear" />
           </div>
         </div>
-        <CitySearch />
+        <CitySearch setLocation={this.setLocation} />
         <div className="d-flex flex-row-reverse">
           <input className="btn btn-primary" type="submit" value="Submit" />
           <button className="btn btn-secondary mr-2" role="button" onClick={this.clearForm}>Clear</button>
