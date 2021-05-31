3c48f7a1e1648d5437e96c4af82f76e5c5f195a2
gabe <ggetzie@gmail.com>
2021-06-01 06:25:00 +0800

    start graph

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,6 +3,7 @@
  * @description Checks to see if the DOM is available by checking the existence of the window and document
  * @see https://github.com/facebook/fbjs/blob/master/packages/fbjs/src/core/ExecutionEnvironment.js#L12
  */
+import { encode } from "ngeohash";
 
 export function isDomAvailable() {
   return typeof window !== 'undefined' && !!window.document && !!window.document.createElement;
@@ -31,3 +32,14 @@ export function isDomAvailable() {
     ]
   ]
  }
+
+//  export class Graph {
+//    constructor() {
+//      this.adjacency_matrix = new Map()
+//    }
+
+//    add_edge(e) {
+
+//    }
+
+//  }
